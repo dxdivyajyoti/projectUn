d1d10d36fa499e67fbfe5d77445ee9343546fd7f
Divyajyoti <b0276648@airtel.com>
2024-04-09 12:36:31 +0530

    java21

--- src/main/java/com/airtel/java21/controller/ZgcController.java
+++ src/main/java/com/airtel/java21/controller/ZgcController.java
@@ -1,20 +1,25 @@
 package com.airtel.java21.controller;
 
+import net.bytebuddy.build.BuildLogger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ZgcController
 {
     @RequestMapping("/zgc")
+
         public String generateMemoryLoad() {
             List<Object> objects = new ArrayList<>();
             for (int i = 0; i < 1000000; i++) {
                 objects.add(new Object());
             }
+//        log.info("Thread info {} ", Thread.currentThread());
             return "Generated " + objects.size() + " objects.";
         }
     }
